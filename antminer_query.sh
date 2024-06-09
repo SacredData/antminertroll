e757863e0d436803df08b54d43348baba3d7d97d
agrathwohl <andrew@grathwohl.me>
2024-06-09 10:37:04 -0500

    write headers to the IP addr file as output by query script

--- antminer_query.sh
+++ antminer_query.sh
@@ -1,8 +1,19 @@
 ips=$@
 
+devs_header=$(echo '{"command": "devs"}' | nc $ips 4028 | jq '.[]?[]? |keys_unsorted| join(",")' | jq -s '[.[]?] | join(",")')
+echo "$devs_header"
 devs_cmd=$(echo '{"command": "devs"}' | nc $ips 4028 | jq '.[]?[]? | join(",")' | jq -s '[.[]?] | join(",")')
+temps_header=$(echo '{"command": "temps"}' | nc $ips 4028 | jq '.[]?[]? |keys_unsorted| join(",")' | jq -s '[.[]?] | join(",")')
+echo "$temps_header"
 temps_cmd=$(echo '{"command": "temps"}' | nc $ips 4028 | jq '.[]?[]? | join(",")' | jq -s '[.[]?] | join(",")')
+details_header=$(echo '{"command": "devdetails"}' | nc $ips 4028 | jq '.[]?[]? |keys_unsorted| join(",")' | jq -s '[.[]] | join(",")')
+echo "$details_header"
 details_cmd=$(echo '{"command": "devdetails"}' | nc $ips 4028 | jq '.[]?[]? | join(",")' | jq -s '[.[]] | join(",")')
+
+if [ ! -f "$ips.csv" ]; then
+  header_fmt="Datetime,$devs_header,$temps_header,$details_header"
+  echo "${header_fmt//\"/}" > "$ips.csv"
+fi
 
 date_now=$(date -u --iso-8601=seconds)
 csv_fmt="$date_now,$devs_cmd,$temps_cmd,$details_cmd"
